c91a59eb233de8433ac2695792c20ea01482deb9
Furkan Balcı <cpn1230@outlook.com>
2025-08-13 15:27:20 +0300

    freeze effect added

--- Assets/Scripts/Powerups/Freeze.cs
+++ Assets/Scripts/Powerups/Freeze.cs
@@ -1,16 +1,31 @@
 using UnityEngine;
 using System;
+using UnityEngine.UI;
+
 public class Freeze : Powerup
 {
     [Header("Elements")] 
     [SerializeField] private Animator animator;
+    [SerializeField] private Image iceFrameImage;
+    
+    [Header("Settings")]
+    [SerializeField] private float fadeDuration = 0.5f;
+    [SerializeField] private float powerupDuration = 5f;
     
     [Header("Actions")] 
     public static Action started;
 
+    private void Awake()
+    {
+        iceFrameImage.gameObject.SetActive(false);
+    }
+
     public void TriggerPowerupStart()
     {
         started?.Invoke();
+        
+        ShowIceFrame();
+        Invoke(nameof(HideIceFrame), powerupDuration);
     }
 
     public void Play()
@@ -18,5 +33,24 @@ public class Freeze : Powerup
         animator.Play("FreezeActive");
         AudioManager.instance.PlayFreeze();
 
+    }
+    
+    private void ShowIceFrame()
+    {
+        iceFrameImage.gameObject.SetActive(true);
+        SetAlpha(0f);
+        LeanTween.alpha(iceFrameImage.rectTransform, 0.6f, fadeDuration);
+    }
+
+    private void HideIceFrame()
+    {
+        LeanTween.alpha(iceFrameImage.rectTransform, 0f, fadeDuration);
+    }
+    
+    private void SetAlpha(float value)
+    {
+        Color c = iceFrameImage.color;
+        c.a = value;
+        iceFrameImage.color = c;
     }
 }
